4ede77d6bb38b5e880872de7b7dce25a40b141fe
Tylpele <117898725+Tylpele@users.noreply.github.com>
2023-11-21 15:24:20 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int[] numerals = {-2, 3,0, -3,4, 5, -3, -4, -5, 5,0, -23, 3, 9};
+        int[] numerals = {-2, -3,0, -3,-4, -5, -3, -4, -5, -5,-55, 23, -3, -9};
         int sizeList= numerals.length;
         List<Integer> numeralList= new LinkedList<>();
         for(int i =0; i<sizeList; i++){
@@ -18,7 +18,7 @@ public class Main {
     public static List<Integer> sortList(List<Integer> numeralList){
 
         int left =0, right = numeralList.size()-1;
-        while (left != right){
+        while (left < right){
             if(numeralList.get(left)<0 && numeralList.get(right)>=0){
                 int temp = numeralList.get(left);
                 numeralList.set(left, numeralList.get(right));
